238609f1596885c02d27a1c02ee2ac4e92c261eb
Daniel Czeschner <daniel.czeschner@web.de>
2022-05-23 19:20:26 +0200

    worked on the simulation objects

--- src/main/java/com/dhbw/thesim/core/entity/Plant.java
+++ src/main/java/com/dhbw/thesim/core/entity/Plant.java
@@ -1,4 +1,18 @@
 package com.dhbw.thesim.core.entity;
 
+/**
+ * It's a plant-object in our simulation, which can be eaten.
+ *
+ * @author Daniel Czeschner
+ */
 public class Plant extends SimulationObject{
+
+    public Plant() {
+        super();
+    }
+
+    @Override
+    public void update(double deltaTime) {
+
+    }
 }
--- src/main/java/com/dhbw/thesim/core/entity/SimulationObject.java
+++ src/main/java/com/dhbw/thesim/core/entity/SimulationObject.java
@@ -1,4 +1,36 @@
 package com.dhbw.thesim.core.entity;
 
+import com.dhbw.thesim.core.util.Vector2D;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+/**
+ * Represents an object, which is handled in our simulation
+ *
+ * @author Daniel Czeschner
+ */
 public abstract class SimulationObject {
+
+    private ImageView imageObj;
+    private Vector2D position;
+
+    public SimulationObject() {
+        imageObj = new ImageView();
+    }
+
+    public abstract void update(double deltaTime);
+
+    //region getter & setter
+    public void setSprite(Image image) {
+        imageObj.setImage(image);
+    }
+
+    public Vector2D getPosition() {
+        return position;
+    }
+
+    public void setPosition(Vector2D position) {
+        this.position = position;
+    }
+    //endregion
 }
