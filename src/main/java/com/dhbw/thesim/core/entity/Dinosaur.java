d805a8adf2e1432217d2f22f406aaa655258a05a
Daniel Czeschner <daniel.czeschner@web.de>
2022-05-23 19:23:52 +0200

    worked on dinosaur

--- src/main/java/com/dhbw/thesim/core/entity/Dinosaur.java
+++ src/main/java/com/dhbw/thesim/core/entity/Dinosaur.java
@@ -1,4 +1,19 @@
 package com.dhbw.thesim.core.entity;
 
+/**
+ * Represents a dinosaur object in our simulation
+ *
+ * @author Daniel Czeschner
+ */
 public class Dinosaur extends SimulationObject{
+
+    public Dinosaur() {
+        super();
+    }
+
+    @Override
+    public void update(double deltaTime) {
+
+    }
+
 }
