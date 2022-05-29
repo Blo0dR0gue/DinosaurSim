899fb7621760b9deb92dbf84886dda8c9beb4b14
Daniel Czeschner <daniel.czeschner@web.de>
2022-05-29 19:42:13 +0200

    Comment update

--- src/main/java/com/dhbw/thesim/core/statemachine/StateMachine.java
+++ src/main/java/com/dhbw/thesim/core/statemachine/StateMachine.java
@@ -5,7 +5,7 @@ import com.dhbw.thesim.core.statemachine.state.State;
 /**
  * This class holds the current State for a dinosaur.
  * It handles the transitions between behaviors.
- * For a deeper look, see the definition of the State Pattern.
+ * For a deeper look, see the definition of the "State Pattern".
  *
  * @author Daniel Czeschner
  * @see State
