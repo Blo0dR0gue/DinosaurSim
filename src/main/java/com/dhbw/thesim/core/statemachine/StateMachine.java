d67aec38a149cd0d319bf4f9e5d32c6f04aa45e9
Daniel Czeschner <daniel.czeschner@web.de>
2022-05-29 19:41:49 +0200

    Comments

--- src/main/java/com/dhbw/thesim/core/statemachine/StateMachine.java
+++ src/main/java/com/dhbw/thesim/core/statemachine/StateMachine.java
@@ -7,11 +7,14 @@ import com.dhbw.thesim.core.statemachine.state.State;
  * It handles the transitions between behaviors.
  * For a deeper look, see the definition of the State Pattern.
  *
- *
  * @author Daniel Czeschner
+ * @see State
  */
 public class StateMachine {
 
+    /**
+     * The current State an {@link com.dhbw.thesim.core.entity.SimulationObject} is in.
+     */
     State currentState;
 
 }
