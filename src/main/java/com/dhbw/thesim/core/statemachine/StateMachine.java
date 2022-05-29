54d759c9a958adaf5b36e7e421e819a7045fc950
Daniel Czeschner <daniel.czeschner@web.de>
2022-05-29 18:55:22 +0200

    Added state machine

---
+++ src/main/java/com/dhbw/thesim/core/statemachine/StateMachine.java
@@ -0,0 +1,17 @@
+package com.dhbw.thesim.core.statemachine;
+
+import com.dhbw.thesim.core.statemachine.state.State;
+
+/**
+ * This class holds the current State for a dinosaur.
+ * It handles the transitions between behaviors.
+ * For a deeper look, see the definition of the State Pattern.
+ *
+ *
+ * @author Daniel Czeschner
+ */
+public class StateMachine {
+
+    State currentState;
+
+}
---
+++ src/main/java/com/dhbw/thesim/core/statemachine/state/State.java
@@ -0,0 +1,13 @@
+package com.dhbw.thesim.core.statemachine.state;
+
+/**
+ * Represents a single State for a dinosaur.
+ * Each state handles one behaviour for a dinosaur.
+ *
+ * @author Daniel Czeschner
+ */
+public abstract class State {
+
+
+
+}
