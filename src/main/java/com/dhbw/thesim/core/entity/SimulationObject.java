3f230f2b796b291f0bafb9e1ec9695dd37bb620e
Daniel Czeschner <daniel.czeschner@web.de>
2022-05-29 19:11:30 +0200

    Comments

--- src/main/java/com/dhbw/thesim/core/entity/SimulationObject.java
+++ src/main/java/com/dhbw/thesim/core/entity/SimulationObject.java
@@ -1,6 +1,7 @@
 package com.dhbw.thesim.core.entity;
 
 import com.dhbw.thesim.core.simulation.Simulation;
+import com.dhbw.thesim.core.simulation.SimulationLoop;
 import com.dhbw.thesim.core.util.Vector2D;
 import javafx.scene.Node;
 import javafx.scene.image.Image;
@@ -13,36 +14,71 @@ import javafx.scene.image.ImageView;
  */
 public abstract class SimulationObject {
 
-    private ImageView imageObj;
-    private Vector2D position;
+    /**
+     * The representation object for this {@link SimulationObject}.
+     * Contains an {@link Image}
+     */
+    protected ImageView imageObj;
+    /**
+     * The position object for this {@link SimulationObject}.
+     * @see Vector2D
+     */
+    protected Vector2D position;
 
+    /**
+     * Constructor
+     */
     public SimulationObject() {
         super();
         imageObj = new ImageView();
+        //TODO
         position = new Vector2D(0,0);
     }
 
+    /**
+     * Is called each update in the {@link SimulationLoop}.
+     * @param deltaTime The time since the last update call in seconds.
+     * @param currentSimulation The current {@link Simulation}-Object with all information to the currently running simulation.
+     */
     public abstract void update(double deltaTime, Simulation currentSimulation);
 
+    /**
+     * Is called each render call. (FPS)
+     */
+    public abstract void updateGraphics();
+
     //region getter & setter
+
+    /**
+     * Sets/Updates and image for the representation of this {@link SimulationObject}
+     * @param image
+     */
     public void setSprite(Image image) {
         imageObj.setImage(image);
     }
 
-    public void updatePosition(){
-        imageObj.setTranslateX(position.getX());
-        imageObj.setTranslateY(position.getY());
-    }
-
+    /**
+     * Gets the current position on the screen. (Doesn't have to be the position of the graphics)
+     * Use this for behaviour updates.
+     * @return The current {@link Vector2D} position.
+     */
     public Vector2D getPosition() {
         return position;
     }
 
+    /**
+     * Sets/Overrides the position of this {@link SimulationObject}
+     * @param position The new {@link Vector2D} for the position.
+     */
     public void setPosition(Vector2D position) {
         this.position = position;
     }
 
-    public Node getJavaFXObj(){
+    /**
+     * Gets the current JavaFX object.
+     * @return An {@link ImageView}. It's the representation for this {@link SimulationObject}
+     */
+    public ImageView getJavaFXObj(){
         return imageObj;
     }
 
