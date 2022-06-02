83deaf0e8899c72b52c217c02b06f576ffcd15b2
Daniel Czeschner <daniel.czeschner@web.de>
2022-06-02 22:37:20 +0200

    Added dinosaur states (TODOS)

---
+++ src/main/java/com/dhbw/thesim/core/statemachine/state/dinosaur/Dead.java
@@ -0,0 +1,33 @@
+package com.dhbw.thesim.core.statemachine.state.dinosaur;
+
+import com.dhbw.thesim.core.entity.Dinosaur;
+import com.dhbw.thesim.core.entity.SimulationObject;
+import com.dhbw.thesim.core.simulation.Simulation;
+import com.dhbw.thesim.core.statemachine.state.State;
+
+/**
+ * TODO
+ *
+ * @author Daniel Czeschner
+ */
+public class Dead extends State {
+
+    /**
+     * Constructor
+     *
+     * @param simulationObject The handled {@link SimulationObject}
+     */
+    public Dead(Dinosaur simulationObject) {
+        super(simulationObject);
+    }
+
+    @Override
+    public void update(double deltaTime, Simulation simulation) {
+
+    }
+
+    @Override
+    public void initTransitions() {
+
+    }
+}
---
+++ src/main/java/com/dhbw/thesim/core/statemachine/state/dinosaur/Mate.java
@@ -0,0 +1,34 @@
+package com.dhbw.thesim.core.statemachine.state.dinosaur;
+
+import com.dhbw.thesim.core.entity.Dinosaur;
+import com.dhbw.thesim.core.entity.SimulationObject;
+import com.dhbw.thesim.core.simulation.Simulation;
+import com.dhbw.thesim.core.statemachine.state.State;
+
+/**
+ * TODO
+ *
+ * @author Daniel Czeschner
+ */
+public class Mate extends State {
+
+    /**
+     * Constructor
+     *
+     * @param simulationObject The handled {@link SimulationObject}
+     */
+    public Mate(Dinosaur simulationObject) {
+        super(simulationObject);
+    }
+
+    @Override
+    public void update(double deltaTime, Simulation simulation) {
+
+    }
+
+    @Override
+    public void initTransitions() {
+
+    }
+
+}
