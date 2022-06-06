133d8891d892b3d2b7add4f1deb2205733fd6857
s200299 <s200299@student.dhbw-mannheim.de>
2022-06-06 20:13:58 +0200

    Added MapListItem fxml and controller

---
+++ src/main/java/com/dhbw/thesim/gui/controllers/MapListItem.java
@@ -0,0 +1,11 @@
+package com.dhbw.thesim.gui.controllers;
+
+import javafx.scene.control.RadioButton;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+
+public class MapListItem {
+    public HBox hbox_outer = null;
+        public RadioButton radioButton = null;
+        public ImageView imageView = null;
+}
