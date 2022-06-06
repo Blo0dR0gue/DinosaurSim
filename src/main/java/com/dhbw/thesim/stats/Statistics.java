bac021b84fc58055c8bb4e5fc1017a140cb883bc
kaigr <kai.gruebener@gmx.net>
2022-06-06 12:53:50 +0200

    Created class Statistics & Method frames // ToDo Abstimmungen mit GUI-Team

---
+++ src/main/java/com/dhbw/thesim/stats/Statistics.java
@@ -0,0 +1,17 @@
+package com.dhbw.thesim.stats;
+
+public class Statistics {
+
+    public Statistics(){}
+
+    //Todo Implement method + set return type
+    public void getSingleStats(){
+
+    }
+
+    //ToDO Implement method + set return type
+    public void getSimulationStats(){
+
+    }
+
+}
