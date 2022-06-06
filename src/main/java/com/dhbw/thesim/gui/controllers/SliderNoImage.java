4c2d184f155a2efcd2a0bff4d9a69e3a0a4fe677
s200299 <s200299@student.dhbw-mannheim.de>
2022-06-06 20:14:22 +0200

    Added SliderNoImage fxml and controller

---
+++ src/main/java/com/dhbw/thesim/gui/controllers/SliderNoImage.java
@@ -0,0 +1,11 @@
+package com.dhbw.thesim.gui.controllers;
+
+import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
+import javafx.scene.layout.VBox;
+
+public class SliderNoImage {
+    public VBox vbox_outer = null;
+        public Label label = null;
+        public Slider slider = null;
+}
