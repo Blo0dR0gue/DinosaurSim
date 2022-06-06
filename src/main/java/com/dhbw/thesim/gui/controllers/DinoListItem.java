1a5f278b062e2c1ff00900887432ee57c160c6b4
s200299 <s200299@student.dhbw-mannheim.de>
2022-06-06 20:11:18 +0200

    Added DinoListItem fxml and controller

---
+++ src/main/java/com/dhbw/thesim/gui/controllers/DinoListItem.java
@@ -0,0 +1,19 @@
+package com.dhbw.thesim.gui.controllers;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+
+public class DinoListItem {
+    public HBox hbox_outer = null;
+        public ImageView imageView = null;
+        public BorderPane borderPane = null;
+            public Label label_name = null;
+            public Slider slider = null;
+            public HBox hbox_inner = null;
+                public Button button_info = null;
+                public Button button_remove = null;
+}
