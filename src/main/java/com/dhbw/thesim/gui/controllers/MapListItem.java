04b2362b0d5dded0848e28b9c833b66fca20de00
Robin2800 <superrobin2800@aol.com>
2022-06-07 21:46:20 +0200

    Added custom control for items in Map ListView

--- src/main/java/com/dhbw/thesim/gui/controllers/MapListItem.java
+++ src/main/java/com/dhbw/thesim/gui/controllers/MapListItem.java
@@ -1,11 +1,59 @@
 package com.dhbw.thesim.gui.controllers;
 
+import com.dhbw.thesim.gui.Display;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.StringProperty;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.control.RadioButton;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 
-public class MapListItem {
-    public HBox hbox_outer = null;
-        public RadioButton radioButton = null;
-        public ImageView imageView = null;
+import java.io.IOException;
+
+public class MapListItem extends HBox {
+    @FXML
+    public HBox container;
+        @FXML
+        public RadioButton radioButton;
+        @FXML
+        public ImageView imageView;
+
+    public MapListItem() {
+
+    }
+
+    public MapListItem newInstance() {
+        return (MapListItem) Display.makeFXMLController("map-list-item.fxml", MapListItem.class);
+    }
+
+    public void initialize(String label, Image image){
+        setText(label);
+        setImage(image);
+    }
+
+    public String getText() {
+        return textProperty().get();
+    }
+
+    public void setText(String value) {
+        textProperty().set(value);
+    }
+
+    public StringProperty textProperty() {
+        return radioButton.textProperty();
+    }
+
+    public Image getImage( ) {
+        return imageProperty().getValue();
+    }
+
+    public void setImage(Image image) {
+        imageProperty().set(image);
+    }
+
+    public ObjectProperty<Image> imageProperty() {
+        return imageView.imageProperty();
+    }
 }
