9da8f2df0bcf34ffed2a7d5ed9d0359c654624cc
Robin2800 <superrobin2800@aol.com>
2022-06-07 21:45:15 +0200

    Added custom control for items in Dino ListView

--- src/main/java/com/dhbw/thesim/gui/controllers/DinoListItem.java
+++ src/main/java/com/dhbw/thesim/gui/controllers/DinoListItem.java
@@ -8,6 +8,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
