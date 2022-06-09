ac63e27560a23eb4b1e7f0c20638c8a9f94c61c7
Robin2800 <superrobin2800@aol.com>
2022-06-09 22:49:54 +0200

    Improved max steps slider + Added comments

--- src/main/java/com/dhbw/thesim/gui/controllers/ConfigScreen.java
+++ src/main/java/com/dhbw/thesim/gui/controllers/ConfigScreen.java
@@ -111,8 +111,18 @@ public class ConfigScreen extends AnchorPane {
             maxRuntime.setVisible(currentSelection.equals(auto));
             maxSteps.setVisible(currentSelection.equals(manual));
         });
+
+        maxSteps.slider.valueProperty()
+                .addListener((observableValue, oldValue, newValue) -> maxSteps.label.textProperty().setValue(
+                        String.valueOf(Math.round(newValue.doubleValue()/10)*10)
+                ));
     }
 
+    /**
+     * Method to populate a given list view with all objects from the {@link ArrayList}
+     * @param objectsList {@link ArrayList} containing all objects to populate the list {@link ListView} with
+     * @param listView The {@link ListView} which should be populated
+     */
     private void populateListView(ArrayList<Object[]> objectsList, ListView<ListItem> listView) {
         for (Object[] objects : objectsList) {
             String name = (String) objects[0];
@@ -126,6 +136,9 @@ public class ConfigScreen extends AnchorPane {
         }
     }
 
+    /**
+     * Method to populate {@link #mapListView} with {@link MapListItem}
+     */
     private void populateMapListView() {
         //Toggle group for the map radio buttons, so only one radio button can be active at a time
         ToggleGroup mapGroup = new ToggleGroup();
@@ -142,7 +155,7 @@ public class ConfigScreen extends AnchorPane {
             mapListView.getItems().add(mapListItem);
         }
     }
-
+    //region getter & setter
     public HashMap<String, Integer> getDinoParams(){
         HashMap<String, Integer> dinos = new HashMap<>();
 
@@ -180,4 +193,6 @@ public class ConfigScreen extends AnchorPane {
     public double getMaxSteps(){
         return maxSteps.getValue();
     }
+
+    //endregion
 }
