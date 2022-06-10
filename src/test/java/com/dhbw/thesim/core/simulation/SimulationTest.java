3a491da1a141402314872ade4881417399a2271f
Daniel Czeschner <daniel.czeschner@web.de>
2022-06-10 08:06:43 +0200

    unit tests

--- src/test/java/com/dhbw/thesim/core/simulation/SimulationTest.java
+++ src/test/java/com/dhbw/thesim/core/simulation/SimulationTest.java
@@ -42,39 +42,79 @@ class SimulationTest {
     }
 
     @Test
-    void sortByDistance(){
+    void sortByDistance() {
 
         List<SimulationObject> simulationObjectList = new ArrayList<>();
 
         Image myObjectMock = mock(Image.class);
 
         Plant p2 = mock(Plant.class);
-        when(p2.getPosition()).thenReturn(new Vector2D(800,800));
+        when(p2.getPosition()).thenReturn(new Vector2D(800, 800));
         when(p2.getInteractionRange()).thenReturn(2d);
         when(p2.getGrowthRate()).thenReturn(2d);
 
         simulationObjectList.add(p2);
 
         Plant p1 = mock(Plant.class);
-        when(p1.getPosition()).thenReturn(new Vector2D(40,40));
+        when(p1.getPosition()).thenReturn(new Vector2D(40, 40));
         when(p1.getInteractionRange()).thenReturn(2d);
         when(p1.getGrowthRate()).thenReturn(2d);
         simulationObjectList.add(p1);
 
-        Vector2D sortTo = new Vector2D(50,50);
+        Vector2D sortTo = new Vector2D(50, 50);
 
-        simulation.sortByDistance(new Vector2D(50,50), simulationObjectList);
+        simulation.sortByDistance(sortTo, simulationObjectList);
 
         double lastDistance = 0;
 
-        for (SimulationObject simulationObject: simulationObjectList) {
+        for (SimulationObject simulationObject : simulationObjectList) {
 
             double distance = Vector2D.distance(sortTo, simulationObject.getPosition());
 
             assertTrue(lastDistance <= distance);
 
+            lastDistance =  distance;
+
         }
 
 
     }
+
+    @Test
+    void sortByDistanceRandomTest() {
+
+        List<SimulationObject> simulationObjectList = new ArrayList<>();
+
+        Image myObjectMock = mock(Image.class);
+
+        Random random = new Random();
+
+        Plant plant;
+        for (int i = 0; i < 50; i++) {
+            plant = mock(Plant.class);
+            when(plant.getPosition()).thenReturn(new Vector2D(random.nextDouble(0, 1860), random.nextDouble(0, 1080)));
+            when(plant.getInteractionRange()).thenReturn(2d);
+            when(plant.getGrowthRate()).thenReturn(2d);
+            simulationObjectList.add(plant);
+        }
+
+        Vector2D sortTo = new Vector2D(random.nextDouble(0, 1860), random.nextDouble(0, 1080));
+
+        simulation.sortByDistance(sortTo, simulationObjectList);
+
+        double lastDistance = 0;
+
+        for (SimulationObject simulationObject : simulationObjectList) {
+
+            double distance = Vector2D.distance(sortTo, simulationObject.getPosition());
+
+            assertTrue(lastDistance <= distance);
+
+            lastDistance = distance;
+
+        }
+
+
+    }
+
 }
