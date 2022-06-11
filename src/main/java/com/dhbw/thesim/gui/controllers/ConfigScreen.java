39b02138c78c7b192a2ed969ad6df149a6f44a4c
Robin2800 <superrobin2800@aol.com>
2022-06-11 18:12:59 +0200

    Moved check for min sim objects into separate method

--- src/main/java/com/dhbw/thesim/gui/controllers/ConfigScreen.java
+++ src/main/java/com/dhbw/thesim/gui/controllers/ConfigScreen.java
@@ -102,37 +102,7 @@ public class ConfigScreen extends AnchorPane {
      */
     public void initializeListeners() {
         // When the start button is clicked the current scene gets replaced by the SimulationOverlay scene
-        startSimulationButton.setOnAction(event -> {
-            boolean minDinosSelected = true;
-            boolean minPlantsSelected = true;
-
-            for (ListView<ListItemWithImage> listView : Arrays.asList(dinoListView, plantListView)) {
-                ArrayList<ListItemWithImage> items = new ArrayList<>();
-                for (ListItemWithImage listItem : listView.getItems()) {
-                    if (listItem.getCount() > 0) {
-                        items.add(listItem);
-                    }
-                }
-                if (items.size() < 2 && listView.equals(dinoListView)) {
-                    minDinosSelected = false;
-                }
-                else if (items.size() < 1 && listView.equals(plantListView)) {
-                    minPlantsSelected = false;
-                }
-            }
-            if (minDinosSelected && minPlantsSelected) {
-                Stage window = (Stage) startSimulationButton.getScene().getWindow();
-                SimulationOverlay simulationOverlay = new SimulationOverlay(window, this);
-                window.setScene(simulationOverlay.getSimulationScene());
-                window.setFullScreen(true);
-            }
-            if (!minDinosSelected) {
-                //TODO Show 'not enough dinos' error popup
-            }
-            if (!minPlantsSelected) {
-                //TODO Show 'no plant' error popup
-            }
-        });
+        startSimulationButton.setOnAction(event -> checkRequiredSimObjects());
 
         // add a change listener
         modeGroup.selectedToggleProperty().addListener((observableValue, previousSelection, currentSelection) -> {
@@ -144,6 +114,38 @@ public class ConfigScreen extends AnchorPane {
                 .addListener((observableValue, oldValue, newValue) -> maxSteps.sliderValueLabel.textProperty().setValue(
                         String.valueOf(Math.round(newValue.doubleValue()/10)*10)
                 ));
+    }
+
+    private void checkRequiredSimObjects() {
+        boolean minDinosSelected = true;
+        boolean minPlantsSelected = true;
+
+        for (ListView<ListItemWithImage> listView : Arrays.asList(dinoListView, plantListView)) {
+            ArrayList<ListItemWithImage> items = new ArrayList<>();
+            for (ListItemWithImage listItem : listView.getItems()) {
+                if (listItem.getCount() > 0) {
+                    items.add(listItem);
+                }
+            }
+            if (items.size() < 2 && listView.equals(dinoListView)) {
+                minDinosSelected = false;
+            }
+            else if (items.size() < 1 && listView.equals(plantListView)) {
+                minPlantsSelected = false;
+            }
+        }
+        if (minDinosSelected && minPlantsSelected) {
+            Stage window = (Stage) startSimulationButton.getScene().getWindow();
+            SimulationOverlay simulationOverlay = new SimulationOverlay(window, this);
+            window.setScene(simulationOverlay.getSimulationScene());
+//            window.setFullScreen(true);
+        }
+        if (!minDinosSelected) {
+            //TODO Show 'not enough dinos' error popup
+        }
+        if (!minPlantsSelected) {
+            //TODO Show 'no plant' error popup
+        }
     }
 
     /**
