84747268ed09dfcc42921b31b57133c95f829555
Daniel Czeschner <daniel.czeschner@web.de>
2022-06-12 10:52:07 +0200

    export all modules

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -6,12 +6,16 @@ module com.dhbw.thesim {
 
     exports com.dhbw.thesim;
     exports com.dhbw.thesim.core.entity;
+    exports com.dhbw.thesim.core.map;
+    exports com.dhbw.thesim.core.statemachine;
+    exports com.dhbw.thesim.core.simulation;
+    exports com.dhbw.thesim.core.util;
+    exports com.dhbw.thesim.core.statemachine.state.dinosaur;
+    exports com.dhbw.thesim.core.statemachine.state.plant;
     exports com.dhbw.thesim.gui;
     exports com.dhbw.thesim.gui.controllers;
     exports com.dhbw.thesim.stats;
     exports com.dhbw.thesim.impexp;
-    exports com.dhbw.thesim.core.simulation;
-    exports com.dhbw.thesim.core.util;
     opens com.dhbw.thesim to javafx.fxml;
     opens com.dhbw.thesim.gui to javafx.fxml;
     opens com.dhbw.thesim.gui.controllers to javafx.fxml;
