833c0b5eee48ad33882bd40b4eea28ad71a79701
s200299 <s200299@student.dhbw-mannheim.de>
2022-07-07 09:45:39 +0200

    add skeleton test class

---
+++ src/test/java/com/dhbw/thesim/stats/StatisticsTest.java
@@ -0,0 +1,19 @@
+package com.dhbw.thesim.stats;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+public class StatisticsTest {
+
+    @BeforeEach
+    void setUp(){
+
+    }
+
+    @AfterEach
+    void tearDown(){
+
+    }
+
+
+}
